495a0b4d842271cfdf096f1c9ecadf5a34db074c
Matt Payne <Payne@MattPayne.org>
2018-04-06 20:51:12 -0500

    Yay the polygon works!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,47 +10,52 @@ import { GoogleMapsAPIWrapper, PolyMouseEvent } from '@agm/core';
 export class AppComponent {
   title = 'app';
   lat = 51.678418;
+  markers: marker[] = [
+    {
+      lat: 51.673858,
+      lng: 7.815982,
+      label: 'A',
+      draggable: true,
+      infowindowContent: 'test'
+    },
+    {
+      lat: 51.373858,
+      lng: 7.215982,
+      label: 'B',
+      draggable: false,
+      infowindowContent: 'test2'
+    },
+    {
+      lat: 51.423858,
+      lng: 7.895982,
+      label: 'C',
+      draggable: true,
+      infowindowContent: 'test3'
+    },
+    {
+      lat: 52.423858,
+      lng: 7.895982,
+      label: 'C',
+      draggable: true,
+      infowindowContent: 'test3'
+    }
+  ];
+
   lng = 7.809007;
-  marker: [
-        {
-          lat: 52.673858,
-          lng: 7.815982,
-          label: '1',
-          draggable: true,
-          infowindowContent: 'test',
-          markerIcon:  'data:image/svg+xml;utf-8, \
-          <svg width="32" height="32" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg"> \
-            <path fill="red" stroke="white" stroke-width="1.5" d="M3.5 3.5h25v25h-25z" ></path> \
-          </svg>',
-          newIcon: ''
-        },
-        {
-          lat: 52.373858,
-          lng: 7.215982,
-          label: '2',
-          draggable: false,
-          infowindowContent: 'test2',
-          markerIcon:  'data:image/svg+xml;utf-8, \
-          <svg width="32" height="32" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg"> \
-            <path fill="red" stroke="white" stroke-width="1.5" d="M3.5 3.5h25v25h-25z" ></path> \
-          </svg>',
-          newIcon: ''
-        },
-        {
-          lat: 52.973858,
-          lng: 7.215982,
-          label: '3',
-          draggable: false,
-          infowindowContent: 'test2',
-          markerIcon:  'data:image/svg+xml;utf-8, \
-          <svg width="32" height="32" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg"> \
-            <path fill="red" stroke="white" stroke-width="1.5" d="M3.5 3.5h25v25h-25z" ></path> \
-          </svg>',
-          newIcon: ''
-        }
-   ];
+  mapTypeId = 'terrain';
 
   polyClick(e: any) {
     console.log(e);
   }
 }
+
+
+// just an interface for type safety.
+interface marker {
+  lat: number;
+  lng: number;
+  label?: string;
+  draggable: boolean;
+  infowindowContent: string;
+}
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { GoogleMapsAPIWrapper } from '@agm/core';
 
 import { AppComponent } from './app.component';
+import { AgmJsMarkerClustererModule } from '@agm/js-marker-clusterer';
 import { AgmCoreModule } from '@agm/core';
 
 
@@ -15,6 +16,7 @@ import { AgmCoreModule } from '@agm/core';
     AgmCoreModule.forRoot({
       apiKey: 'AIzaSyAqxjtsvw8b-EVZ1sPk1ZJ8dh8jKl7eDU0'
     }),
+    AgmJsMarkerClustererModule
   ],
   providers: [GoogleMapsAPIWrapper],
   bootstrap: [AppComponent]
