d95d90fbf11c4187af365c5ebb402b96798e8c01
Matt Payne <Payne@MattPayne.org>
2018-04-06 19:13:40 -0500

    broken

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,45 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  lat: number = 51.678418;
+  lng: number = 7.809007;
+
+      marker : [
+        {
+          lat: 52.673858,
+          lng: 7.815982,
+          label: '1',
+          draggable: true,
+          infowindowContent: 'test',
+          markerIcon :  'data:image/svg+xml;utf-8, \
+          <svg width="32" height="32" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg"> \
+            <path fill="red" stroke="white" stroke-width="1.5" d="M3.5 3.5h25v25h-25z" ></path> \
+          </svg>',
+          newIcon: ''
+        },
+        {
+          lat: 52.373858,
+          lng: 7.215982,
+          label: '2',
+          draggable: false,
+          infowindowContent: 'test2',
+          markerIcon :  'data:image/svg+xml;utf-8, \
+          <svg width="32" height="32" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg"> \
+            <path fill="red" stroke="white" stroke-width="1.5" d="M3.5 3.5h25v25h-25z" ></path> \
+          </svg>',
+          newIcon: ''
+        },
+        {
+          lat: 52.973858,
+          lng: 7.215982,
+          label: '3',
+          draggable: false,
+          infowindowContent: 'test2',
+          markerIcon :  'data:image/svg+xml;utf-8, \
+          <svg width="32" height="32" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg"> \
+            <path fill="red" stroke="white" stroke-width="1.5" d="M3.5 3.5h25v25h-25z" ></path> \
+          </svg>',
+          newIcon: ''
+        }
+      ]
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { GoogleMapsAPIWrapper } from '@agm/core';
 
 
 import { AppComponent } from './app.component';
+import { AgmCoreModule } from '@agm/core';
 
 
 @NgModule({
@@ -10,9 +12,12 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyAqxjtsvw8b-EVZ1sPk1ZJ8dh8jKl7eDU0'
+    }),
   ],
-  providers: [],
+  providers: [GoogleMapsAPIWrapper],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
